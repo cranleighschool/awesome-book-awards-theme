fae24e65ddd2497e83c8525d678d99e31d2cdcd0
Fred Bradley <hello@fredbradley.co.uk>
2017-10-02 12:44:31 +0100

    Trying out new style for news

--- loop-templates/content.php
+++ loop-templates/content.php
@@ -8,43 +8,52 @@
 ?>
 
 <article <?php post_class('archive-article'); ?> id="post-<?php the_ID(); ?>">
+	<div class="row">
+		<div class="col-sm-4">
+			<a class="post-featured-image" href="<?php echo get_permalink(); ?>">
+				<?php echo get_the_post_thumbnail($post->ID, 'author-mugshot'); ?>
+			</a>
+		</div>
+		<div class="col-sm-8">
 
-	<header class="entry-header">
+			<header class="entry-header">
 
-		<?php if ( 'post' == get_post_type() ) : ?>
+				<?php if ( 'post' == get_post_type() ) : ?>
 
-			<div class="entry-meta">
-				<?php understrap_posted_on(); ?>
-			</div><!-- .entry-meta -->
+					<div class="entry-meta">
+						<?php understrap_posted_on(); ?>
+					</div><!-- .entry-meta -->
 
-		<?php endif; ?>
+				<?php endif; ?>
 
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
-		'</a></h2>' ); ?>
+				<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
+					'</a></h2>' ); ?>
 
-	</header><!-- .entry-header -->
+			</header><!-- .entry-header -->
 
 
-	<div class="entry-content">
-		<?php echo get_the_post_thumbnail( $post->ID, 'author-mugshot' ); ?>
+			<div class="entry-content">
+				<?php
+				the_excerpt();
+				?>
+				<?php
+				wp_link_pages( array(
+					'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
+					'after'  => '</div>',
+				) );
+				?>
 
-		<?php
-		the_excerpt();
-		?>
-		<?php
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
-			'after'  => '</div>',
-		) );
-		?>
+				<div class="clear clearfix">&nbsp;</div>
+			</div><!-- .entry-content -->
 
-		<div class="clear clearfix">&nbsp;</div>
-	</div><!-- .entry-content -->
+			<footer class="entry-footer">
 
-	<footer class="entry-footer">
+				<?php understrap_entry_footer(); ?>
 
-		<?php understrap_entry_footer(); ?>
+			</footer><!-- .entry-footer -->
 
-	</footer><!-- .entry-footer -->
+		</div>
+	</div>
+
 
 </article><!-- #post-## -->
