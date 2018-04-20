424798b3e2212015c06449b20904ebef13ccc983
Fred Bradley <hello@fredbradley.co.uk>
2018-04-20 13:00:24 +0100

    Make it obvious that this is new

--- app/Widgets/DashboardVotingInformation.php
+++ app/Widgets/DashboardVotingInformation.php
@@ -26,7 +26,7 @@ class DashboardVotingInformation  {
     public function setup_dashboard() {
         wp_add_dashboard_widget(
             'awesome_voting_information', // Widget Slug.
-            'Voting Information', // Title
+            'Voting Information <span style="color:red;">[NEW]</span>', // Title
             array($this, 'widget')
         );
     }
